66eca17a4ed23a632bdfe4485dcff5b744919015
ashab <asha.ab@gmail.com>
2015-09-03 16:07:03 -0400

    Module to reverse geocode lat/long using Google Maps API

---
+++ cloudCode/cloud/geocode.js
@@ -0,0 +1,21 @@
+
+Parse.Cloud.define('getLocationNameFromCoordinate', function(request, response) {
+	var latlng = request.params.latitude + "," + request.params.longitude;
+	Parse.Cloud.httpRequest({
+		url: 'https://maps.googleapis.com/maps/api/geocode/json',
+		params: {
+			latlng: latlng,
+			key: 'AIzaSyDVCuND4-X9GSPEPf2_SiI5Yc0mUZ39B30'
+		}
+	}).then(function(httpResponse) {
+		var parsedResponse = httpResponse.data;
+		if (parsedResponse.status === "OK") {
+			var locationName = parsedResponse.results[0].formatted_address;
+			response.success(locationName);
+		} else {
+			response.error("Could not fetch results " + httpResponse.status)
+		}
+	},function(httpResponse) {
+		response.error('Request failed with response code ' + httpResponse.status)
+	});
+});
---
+++ cloudCode/cloud/main.js
@@ -0,0 +1,1 @@
+var geocodeService = require('cloud/geocode.js');
